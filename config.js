95256ae60f26c6624fa1934b3f9309527efbef69
🦅NEXA📜 <144521464+henrygokugt99@users.noreply.github.com>
2025-02-04 08:34:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://nextice401:<dbhenrygokugt@database.s0kvh.mongodb.net/?retryWrites=true&w=majority&appName=database",
-  SESSION_NAME: process.env.SESSION_NAME || "PUBG~"
+  SESSION_NAME: process.env.SESSION_NAME || "PUBG"
 };
